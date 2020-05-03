670409c190d9b0dfd15313bac63b4ab70ee98f78
Fernando Terra <nandobrt@gmail.com>
2020-05-03 16:23:18 -0300

    feat: lessons 1-3

---
+++ src/initial-data.js
@@ -0,0 +1,18 @@
+const data = {
+  tasks: {
+    "task-1": {id: "task-1", content: "do 1"},
+    "task-2": {id: "task-2", content: "do 2"},
+    "task-3": {id: "task-3", content: "do 3"},
+    "task-4": {id: "task-4", content: "do 4"},
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "To do",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"]
+    }
+  },
+  columnOrder: ["column-1"]
+}
+
+export default data;
