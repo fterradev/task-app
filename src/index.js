8c6fda41def4e0d1b47fddbad27787d0525ff134
Fernando Terra <nandobrt@gmail.com>
2020-05-03 17:57:28 -0300

    lesson 4: Reorder a List with react-beautiful-dnd

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "@atlaskit/css-reset"
+import React from "react";
+import ReactDOM from "react-dom";
+import { DragDropContext } from "react-beautiful-dnd";
+import "@atlaskit/css-reset";
 import initialData from "./initial-data";
 import Column from "./column";
 
 class App extends React.Component {
   state = initialData;
-  render() {
-    return this.state.columnOrder.map((columnId) => {
-      const column = this.state.columns[columnId];
-      const tasks = column.taskIds.map((taskId) => this.state.tasks[taskId]);
 
-      return <Column key={column.id} column={column} tasks={tasks} />;
-    })
+  onDragEnd = (result) => {
+    //TODO: reorder our column
+  };
+
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map((columnId) => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(
+            (taskId) => this.state.tasks[taskId]
+          );
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
   }
 }
 
@@ -20,5 +32,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- src/task.jsx
+++ src/task.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
   border: 1px solid lightgrey;
@@ -10,9 +11,17 @@ const Container = styled.div`
 class Task extends React.Component {
   render() {
     return (
-      <Container>
-        {this.props.task.content}
-      </Container>
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided) => (
+          <Container
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
     );
   }
 }
